80df2097cc040397c65b8211a0f1702bec60faea
Mattias Sämskar <mattias.samskar@gmail.com>
2020-05-19 18:48:59 +0200

    Add wwwhisper

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const compression = require("compression");
+const wwwhisper = require("connect-wwwhisper");
 const db = require("./db");
 
 const app = express();
+if (process.env.NODE_ENV === "production") {
+  app.use(wwwhisper());
+}
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(express.static(path.join(__dirname, `/../build`)));
 
 app.get("/notes", async (req, res) => {
