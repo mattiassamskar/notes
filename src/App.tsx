432edb47f487bba6cd6366a8c109df23a3fcc8c4
Mattias Sämskar <mattias.samskar@gmail.com>
2020-05-24 09:13:22 +0200

    Add tab funcionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
-import "./App.css";
+import Nav from "react-bootstrap/Nav";
 import { guid } from "./utils";
 import { NotesTab, NoteData } from "./types";
 import { Note } from "./Note";
@@ -18,6 +18,7 @@ import {
   switchNoteOrder,
   updateNotePosition,
   fetchTabs,
+  saveTab,
 } from "./api";
 import {
   getPreviousNote,
@@ -109,48 +110,86 @@ function App() {
     await getNotes();
   };
 
+  const addTab = async () => {
+    await saveTab({
+      id: guid(),
+      title: "New tab",
+      index: tabs.length + 1,
+    });
+    await getTabs();
+  };
+
   if (tabs.length === 0) return <div>Loading..</div>;
 
   return (
-    <Container fluid>
-      <Tabs
+    <Container fluid className="mt-1">
+      <Tab.Container
         id="tabs"
         activeKey={activeTab}
         onSelect={(tabId: string) => setActiveTab(tabId)}
       >
-        {tabs.map((tab) => (
-          <Tab key={tab.id} eventKey={tab.id} title={tab.title}>
-            <Row>
-              {columns.map((column) => (
-                <Col key={column}>
-                  {getNotesForColumn(notes, tab.id, column).map((note) => (
-                    <Note
-                      key={note.id}
-                      note={note}
-                      save={save}
-                      remove={remove}
-                      moveUp={() => moveUp(note)}
-                      moveRight={moveRight}
-                      moveLeft={moveLeft}
-                    />
-                  ))}
-                  <div className="mt-4 mb-4 fading">
-                    <FontAwesomeIcon
-                      style={{ alignSelf: "center", width: "100%" }}
-                      cursor="pointer"
-                      color={"black"}
-                      icon={faPlus}
-                      onClick={() => addNote(tab.id, column)}
-                    />
-                  </div>
-                </Col>
-              ))}
-            </Row>
-          </Tab>
-        ))}
-      </Tabs>
+        <Nav variant="tabs">
+          {tabs.map((tab) => (
+            <NavItem key={tab.id} id={tab.id} title={tab.title} />
+          ))}
+          <NewTabNavItem onClick={addTab} />
+        </Nav>
+        <Tab.Content>
+          {tabs.map((tab) => (
+            <Tab.Pane key={tab.id} eventKey={tab.id}>
+              <Row>
+                {columns.map((column) => (
+                  <Col key={column}>
+                    {getNotesForColumn(notes, tab.id, column).map((note) => (
+                      <Note
+                        key={note.id}
+                        note={note}
+                        save={save}
+                        remove={remove}
+                        moveUp={() => moveUp(note)}
+                        moveRight={moveRight}
+                        moveLeft={moveLeft}
+                      />
+                    ))}
+                    <div className="mt-4 mb-4 fading">
+                      <FontAwesomeIcon
+                        style={{ alignSelf: "center", width: "100%" }}
+                        cursor="pointer"
+                        color={"black"}
+                        icon={faPlus}
+                        onClick={() => addNote(tab.id, column)}
+                      />
+                    </div>
+                  </Col>
+                ))}
+              </Row>
+            </Tab.Pane>
+          ))}
+        </Tab.Content>
+      </Tab.Container>
     </Container>
   );
 }
 
 export default App;
+
+const NavItem = ({ id, title }: { id: string; title: string }) => (
+  <Nav.Item key={id}>
+    <Nav.Link eventKey={id}>{title}</Nav.Link>
+  </Nav.Item>
+);
+
+const NewTabNavItem = ({ onClick }: { onClick: () => void }) => (
+  <Nav.Item key={"999"}>
+    <Nav.Link eventKey={"999"}>
+      <div className="fading">
+        <FontAwesomeIcon
+          cursor="pointer"
+          color={"black"}
+          icon={faPlus}
+          onClick={onClick}
+        />
+      </div>
+    </Nav.Link>
+  </Nav.Item>
+);
