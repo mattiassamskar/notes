7792f535c5d64444c2e5bab43e6bd9e519145334
Mattias Sämskar <mattias.samskar@gmail.com>
2021-01-16 11:31:25 +0100

    Add build folder

---
+++ build/precache-manifest.9dd6ed78ecafed95c3aa5f1c30a738c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f52c897891f78536303ac89b6c62ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98d80f24ca9a5ca3220a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3c6b153346d41e71eac7",
+    "url": "/static/css/main.7db81ac1.chunk.css"
+  },
+  {
+    "revision": "98d80f24ca9a5ca3220a",
+    "url": "/static/js/2.a08db749.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.a08db749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6b153346d41e71eac7",
+    "url": "/static/js/main.bf1359d8.chunk.js"
+  },
+  {
+    "revision": "c7d7e9b93f62717f3d90",
+    "url": "/static/js/runtime-main.2b8a05ef.js"
+  }
+]);
